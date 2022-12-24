5545e48f61e540292bb6747a9175d2cb6db0002f
Harrison Noh <harrisonnoh@Harrisons-MacBook-Pro.local>
2022-12-24 11:41:59 -0500

    more changes

--- src/jsMain/kotlin/Header.kt
+++
@@ -1,7 +0,0 @@
-import androidx.compose.runtime.Composable
-import org.jetbrains.compose.web.dom.Div
-
-@Composable
-fun Header() {
-    Div {  }
-}
--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,3 +1,4 @@
+import Banner.Banner
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
